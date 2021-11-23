bcd3c75b481178c5c1f71ebf861829afe2940ac9
Chandrakb1 <60431330+Chandrakb1@users.noreply.github.com>
2021-11-23 14:32:19 +0530

    Add files via upload

---
+++ Q1_Fibonacci_9.py
@@ -0,0 +1,27 @@
+# Program to display the Fibonacci numbers first 9 series.
+
+a = 0
+b = 1
+c = 0
+count = 0
+print(a)
+print(b)
+while count < (9-2):
+    c = a + b
+    a = b
+    b = c
+    print(c)
+    count+= 1
+    
+
+
+
+
+      
+
+
+       
+
+
+
+       
